c74c29c266a9441e236e6ee37eb31ab6f2410beb
Steve Njuguna <stevenjuguna93@gmail.com>
2021-08-28 13:11:43 +0300

    Added Transactions model

--- src/API/models.py
+++ src/API/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import uuid
+
+from django.db.models.fields import UUIDField
+
+TRANSACTION_TYPE = [
+    ('Debit', ('Debit')),
+    ('Credit', ('Credit')),
+]
+
+CURRENCY = [
+    ('KSH', ('KSH')),
+    ('TSH', ('TSH')),
+    ('USH', ('USH'))
+]
 
 # Create your models here.
+class Transcations(models.Model):
+    transaction_id = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False, max_length=50)
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE, max_length=50)
+    transaction_currency = models.CharField(choices=CURRENCY, max_length=50)
+    transaction_amount = models.FloatField()
+    merchant_id = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False, max_length=50)
+    merchant_name = models.CharField(max_length=50)
+    customer_id = models.CharField(default=uuid.uuid4, unique=True, db_index=True, editable=False, max_length=50)
+    customer_name = models.CharField(max_length=50)
+    transaction_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.transaction_id()
